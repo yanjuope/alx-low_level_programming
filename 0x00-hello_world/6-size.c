41157655cc29c80dcbd48e743fbbe283f9514aa1
yanjuope <opeye97@gmail.com>
2022-10-17 11:46:54 -0700

    your commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main -is the entry point for the program.
- *
- *Return:zero if no error, non-zero value if errors.
- */
+*main -is the entry point for the program.
+*
+*Return:zero if no error, non-zero value if errors.
+*/
 int main(void)
 {
 printf("Size of a char:%ld byte(s)\n", sizeof(char));
@@ -11,5 +11,5 @@ printf("Size of an int: %ld byte(s)\n", sizeof(int));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 printf("Size of a long  long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
- return (0);
+return (0);
 }
