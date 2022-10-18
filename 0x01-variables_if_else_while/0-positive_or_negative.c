2ede46ac16af42f992fa935fb4d183026221548a
yanjuope <opeye97@gmail.com>
2022-10-18 03:20:07 -0700

    your commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-* main-program entry point.
-* Retutn:0-no error,non zero value if error.
+*main-program entry point.
+*Retutn:0-no error,non zero value if error.
 **/
 int main(void)
 {
