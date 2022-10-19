6f4d0d79199498634eaf1825e162edc3a46b1315
yanjuope <opeye97@gmail.com>
2022-10-19 06:41:30 -0700

    your commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,17 +2,17 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main-program entry point.
-*
-*Return:0 if no error, non-zero value if error.
-*/
+ *main-program entry point.
+ *
+ *Return:0 if no error, non-zero value if error.
+ */
 int main(void)
 {
 int n;
 int ldigit;
 
 srand(time(0));
-n = rand() -RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 ldigit = n % 10;
 if (ldigit > 5)
 {
