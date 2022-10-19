4d759ca3fea302fec33029406d31bee5f620cfe3
yanjuope <opeye97@gmail.com>
2022-10-19 06:45:05 -0700

    your commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ else if (ldigit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, ldigit);
 }
-else if (ldigit < 6 && ldigit !=0)
+else if (ldigit < 6 && ldigit != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ldigit);
 }
