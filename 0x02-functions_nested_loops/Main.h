295004ad6202d5667cdb16f4506b31a30933f0c5
yanjuope <opeye97@gmail.com>
2022-10-24 03:45:13 -0700

    your commit

--- 0x02-functions_nested_loops/Main.h
+++ 0x02-functions_nested_loops/Main.h
@@ -1,9 +1,8 @@
 #ifndef main_h
 #define main_h
 extern int _putchar(char c);
-extern void print_alphabet(void)
-extern void
-print_alphabet_x10(void)
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void)
 extern int _islower(int c);
 extern int _isalpha(int c);
 extern int print_sign(int n);
