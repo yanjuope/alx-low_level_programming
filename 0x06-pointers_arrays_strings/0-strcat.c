a1adfccca335969f8df554062b579746a9e980da
yanjuope <opeye97@gmail.com>
2022-10-26 14:31:36 -0700

    your commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings.
- *
- * @dest: destination.
- * @src: source.
- * Return: the pointer to dest.
- *
- */
+*_strcat - concatenates two strings.
+*
+* 
+*@dest: destination.
+*@src: source.
+*Return: the pointer to dest.
+*
+*/
 char *_strcat(char *dest, char *src)
 {
 int count = 0, count2 = 0;
