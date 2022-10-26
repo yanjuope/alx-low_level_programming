78197a0a89f4077be83c1e153957a4851d1128ca
yanjuope <opeye97@gmail.com>
2022-10-26 08:37:17 -0700

    your commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_array - prints n elements of an array of integers.
- * @a: input array.
- * @n: input n elements
- * Return: no return.
- */
+* print_array - prints n elements of an array of integers.
+* @a: input array.
+* @n: input n elements
+* Return: no return.
+*/
 void print_array(int *a, int n)
 {
 int i = 0;
