872a3af3223ec5832c549ed6ae37fb6d6f227bbb
yanjuope <opeye97@gmail.com>
2022-11-26 07:01:47 -0800

    your commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,12 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n);
 {
 unsigned int index;
 char *memory = s, value = b;
 
-for (index = 0; index < n; index++)
+for (index = 0; index < n; index++);
 	memory[index] = value;
 
 return (memory);
